d98df3098581c81a59c4b0a41da8f3d2c9c7144f
Dedukhin Alexandr <vyshky2@gmail.com>
2025-10-01 13:52:15 +0400

    Исправил ошибки в билде проекта, теперь он правильно удаляет папку и запускает дебаг

--- build.bat
+++ build.bat
@@ -1,3 +1,8 @@
-rm -r -fo build
+@REM Удаляем папку build если есть
+rd /s /q "build"
+
+@REM Генерим build через cmake
 cmake -S . -B build -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Debug
-mingw32-make -C build -j
+
+@REM Компилим проект
+C:/msys64/mingw64/bin/mingw32-make.exe -C build -j
